2ec6fed99d383c4eb55c0a27b4f48a5f678a2798
Pierre-Louis <pierre-louis@tryriot.com>
2022-03-15 15:51:08 +0100

    codegen: use ts files for graphql composition queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      "dark-blue": "rgb(9, 1, 49)",
+    extend: {
+      colors: {
+        "dark-blue": "rgb(9, 1, 49)",
+      },
     },
   },
   plugins: [],
